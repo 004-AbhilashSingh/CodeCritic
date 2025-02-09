c6012ec231580a801d9dc78659e83dbabab0cc96
Abhilash Singh <abhilashsingh326@gmail.com>
2025-02-09 13:37:10 +0530

    Angular and SpringBoot Project Setup

---
+++ src/main/java/com/codeCritic/codeCritic/CodeCriticApplication.java
@@ -0,0 +1,13 @@
+package com.codeCritic.codeCritic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCriticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCriticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeCritic/codeCritic/CodeCriticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeCritic.codeCritic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCriticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
