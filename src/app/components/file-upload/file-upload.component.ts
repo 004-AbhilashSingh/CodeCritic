bb7c4e2fa1df46b0443a8eea3e67d43b43709e50
Abhilash Singh <abhilashsingh326@gmail.com>
2025-02-16 16:56:56 +0530

    UI for code upload/paste

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,27 +1,43 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
-import { UploadEvent } from '../../interfaces/upload-event';
-import { FileUploadEvent } from 'primeng/fileupload';
 import { ButtonModule } from 'primeng/button';
 import { FileUpload } from 'primeng/fileupload';
 import { ToastModule } from 'primeng/toast';
 import { CardModule } from 'primeng/card';
+import { FormsModule } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
+import { FloatLabel } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-file-upload',
   standalone: true,
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.css',
-  imports: [ButtonModule, FileUpload, ToastModule, CardModule],
+  imports: [ButtonModule, FileUpload, ToastModule, CardModule, TextareaModule, FormsModule, FloatLabel],
   providers: [MessageService]
 })
 export class FileUploadComponent {
+
+  file:File | null = null;
+  code:string = '';
 
   constructor(private _messageService:MessageService) {
 
   }
 
-  onUpload(event: FileUploadEvent) {
-    // this._messageService.add({severity: 'info', summary: 'Success', detail: 'Your file has been uploaded successfully'});
+  onUpload(event: any) {
+    this.file = event.files[0];
+    this._messageService.add({severity: 'success', summary: 'File Uploaded', detail: ''});
+  }
+
+  processFile() {
+    if (this.file) {
+      const reader = new FileReader();
+      reader.readAsText(this.file);
+      reader.onloadend = () => {
+        this._messageService.add({severity: 'info', summary: 'File Processed', detail: ''});
+        this.code = reader.result as string;
+      }
+    }
   }
 }
